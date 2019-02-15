6e4bd85bea4c2fd751419e5531a03a669adcb30b
Laptop\Calvin <99045362@mydavinci.nl>
2019-02-15 11:05:00 +0100

    Restarted project... Made 2 arrays with pizzas and topping and create a function that show the names of the pizzas

--- script.js
+++ script.js
@@ -1,73 +1,38 @@
 /* Pizzas in een array zetten */
 var pizzas = [
-	{pizzaid:1,naam:"Pizza BBQ Bacon",foto:"images/pizza1.png",prijs:5.99,},
-	{pizzaid:2,naam:"Pizza American Supreme Meatlover",foto:"images/pizza2.png",prijs:9.95,},
-	{pizzaid:3,naam:"Pizza Double Cheese & Onion",foto:"images/pizza3.png",prijs:4.95,},
-	{pizzaid:4,naam:"Pizza Chili Cheese & Beef",foto:"images/pizza4.png",prijs:5.95,}
+	{naam:"Pizza BBQ Bacon",foto:"images/pizza1.png",prijs:5.99,},
+	{naam:"Pizza American Supreme Meatlover",foto:"images/pizza2.png",prijs:9.95,},
+	{naam:"Pizza Double Cheese & Onion",foto:"images/pizza3.png",prijs:4.95,},
+	{naam:"Pizza Chili Cheese & Beef",foto:"images/pizza4.png",prijs:5.95,}
 ];
 
 /* Toppings in een array zetten */
 var toppings = [
-	{naam:"Bacon",foto:"images/bacon.png",prijs:0.99,choose:false,},
-	{naam:"Kaas",foto:"images/kaas.jpg",prijs:0.49,choose:false,},
-	{naam:"Tomaat",foto:"images/tomaat.png",prijs:0.49,choose:false,},
-	{naam:"Uien",foto:"images/uien.png",prijs:0.49,choose:false,},
-	{naam:"Vlees",foto:"images/vlees.png",prijs:0.99,choose:false,}
+	{naam:"Bacon",prijs:0.99,},
+	{naam:"Kaas",prijs:0.49,},
+	{naam:"Tomaat",prijs:0.49,},
+	{naam:"Uien",prijs:0.49,},
 ];
 
-/* Functie maken en de namen uitlezen */
-function showPizzaName() {
-	var text = "";
-	var i;
-		for (i = 0; i < pizzas.length; i++){
-			text += "(" + pizzas[i].pizzaid + ")" + " " + pizzas[i].naam +"<br>";
-		}
-	document.getElementById("pizzanaam").innerHTML = text;
-} 
-showPizzaName();
 
-/* Functie maken en de foto tonen */
-function showPizzaPicture() {
-	var src = "";
-	var i = 0;
-	var pizzaid = pizzas[i++].pizzaid;
-		if (pizzaid == 1) {
-			src += pizzas[0].foto;
-		}
-		else if (pizzaid == 2) {
-			src += pizzas[1].foto;
-		}
-		else if (pizzaid == 3) {
-			src += pizzas[2].foto;
-		}
-		else if (pizzaid == 4) {
-			src += pizzas[3].foto;
-		}			
- 	document.getElementById("pizzaimage").src = src;
+/* Pizza lijst maken doormiddel van een function */
+function showPizzaList() {
+	/* Unordered list toevoegen in HTML met createElement */
+    var pizzaElement = document.createElement('ul');
+    /* Bepalen waar de unordered list moet worden toegevoegd */
+    	document.getElementById('pizza-list').appendChild(pizzaElement);
+    /* De 4 pizzas loopen door een forloop en dan een li en a aanmaken per pizza */	
+    for (var i = 0; i < pizzas.length; i++) {
+        var pizzaList = document.createElement('li');
+        var pizzaLink = document.createElement('a');
+        pizzaLink.innerHTML = pizzas[i].naam;
+    /* Onclick toevoegen op de pizza met verschillende ID's doormiddel van setAttribute */    
+        pizzaLink.setAttribute("onclick", `showSpecificPizza(${i})`);
+        pizzaElement.appendChild(pizzaList);
+        pizzaList.appendChild(pizzaLink);
+    }
 }
-
-/* Functie maken en de toppings tonen */
-function showPizzaToppings() {
-	var text = "";
-	var t;
-	for (t = 0; t < toppings.length; t++){
-		text += toppings[t].naam + " " + "€" + toppings[t].prijs + "<br>";
-	}
-	document.getElementById("pizzatoppings").innerHTML = text;
-	document.getElementById("pizzatoppings").style.color = "red";
-}
-
-/* Functie maken en de toppings selecteren */
-function checkToppings() {
-	var i = 0;
-	var choose = toppings[i++].choose;
-		if (choose == true) {
-			document.getElementById("pizzatoppings").style.color = "green";
-		} 
-		else {
-			document.getElementById("pizzatoppings").style.color = "red";
-		}
-
-}
+/* Functie bij het laden van de pagina uitvoeren */
+showPizzaList();    
 
 
